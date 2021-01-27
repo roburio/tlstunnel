a03b92545e54b9e5c63167c3e0b2267d0e257bf9
Hannes Mehnert <hannes@mehnert.org>
2021-01-27 18:29:39 +0100

    metrics and monitoring on separate interface

--- config.ml
+++ config.ml
@@ -30,6 +30,18 @@ let key_seed =
   let doc = Key.Arg.info ~doc:"certificate key seed" ["key-seed"] in
   Key.(create "key-seed" Arg.(required string doc))
 
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt (some ip_address) None doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt (some ip_address) None doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "tlstunnel" doc))
+
 let main =
   foreign
     ~keys:[ Key.abstract frontend_port ;
@@ -39,22 +51,30 @@ let main =
             Key.abstract dns_server ;
             Key.abstract domains ;
             Key.abstract key_seed ;
+            Key.abstract monitor ;
+            Key.abstract syslog ;
+            Key.abstract name ;
           ]
     ~packages:[
       package ~min:"0.13.1" "tls-mirage" ;
       package ~min:"5.0.1" ~sublibs:["mirage"] "dns-certify" ;
+      package ~min:"0.0.2" "monitoring-experiments" ;
+      package ~min:"0.3.0" ~sublibs:["mirage"] "logs-syslog" ;
     ]
     "Unikernel.Main"
-    (random @-> time @-> pclock @-> block @-> stackv4v6 @-> stackv4v6 @-> job)
+    (console @-> random @-> time @-> pclock @-> block @-> stackv4v6 @-> stackv4v6 @-> stackv4v6 @-> job)
 
 let stack = generic_stackv4v6 default_network
 
 let private_stack =
   generic_stackv4v6 ~group:"private" (netif ~group:"private" "private")
 
+let management_stack =
+  generic_stackv4v6 ~group:"management" (netif ~group:"management" "management")
+
 let block =
   Key.(if_impl is_solo5 (block_of_file "storage") (block_of_file "disk.img"))
 
 let () =
   register "tlstunnel"
-    [ main $ default_random $ default_time $ default_posix_clock $ block $ stack $ private_stack ]
+    [ main $ default_console $ default_random $ default_time $ default_posix_clock $ block $ stack $ private_stack $ management_stack ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,14 +1,39 @@
 (* (c) 2020 Hannes Mehnert, all rights reserved *)
 
 (* left to do:
-   - statistics (metrics)
    - haproxy1 support (PROXY TCP4|6 SOURCEIP DESTIP SRCPORT DESTPORT\r\n) at the beginning of the TCP connection to the backend
    - NG: apart from SNI allow other ports to be redirected (no proxy)
 *)
 
 open Lwt.Infix
 
-module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCLOCK) (Block : Mirage_block.S) (Public : Mirage_stack.V4V6) (Private : Mirage_stack.V4V6) = struct
+module Main (C : Mirage_console.S) (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCLOCK) (Block : Mirage_block.S) (Public : Mirage_stack.V4V6) (Private : Mirage_stack.V4V6) (Management : Mirage_stack.V4V6) = struct
+  let snis =
+    Monitoring_experiments.counter_metrics ~f:(fun x -> x) "tlstunnel"
+
+  let access kind =
+    let s = ref (0, 0) in
+    let open Metrics in
+    let doc = "connection statistics" in
+    let data () =
+      Data.v [
+        int "active" (fst !s) ;
+        int "total" (snd !s) ;
+      ] in
+    let tags = Tags.string "kind" in
+    let src = Src.v ~doc ~tags:Tags.[ tags ] ~data "connections" in
+    (fun action ->
+       (match action with
+        | `Open -> s := (succ (fst !s), succ (snd !s))
+        | `Close -> s := (pred (fst !s), snd !s));
+       Metrics.add src (fun x -> x kind) (fun d -> d ()))
+
+  let frontend_access = access "frontend"
+  let tls_access = access "tls"
+  let config_access = access "config"
+  let http_access = access "http"
+  let backend_access = access "backend"
+
   module FS = Filesystem.Make(Pclock)(Block)
 
   module M = Map.Make(String)
@@ -102,6 +127,7 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
     | Some data -> handle_command block config data
 
   let config_change block config key tcp =
+    config_access `Open;
     (Private.TCP.read tcp >>= function
       | Error e ->
         Logs.err (fun m -> m "config TCP read error %a" Private.TCP.pp_error e);
@@ -125,6 +151,7 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
           Lwt.return_unit
         end)
     >>= fun () ->
+    config_access `Close;
     Private.TCP.close tcp
 
   module TLS = Tls_mirage.Make(Public.TCP)
@@ -164,6 +191,7 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
       None
 
   let redirect tcp =
+    http_access `Open;
     Public.TCP.read tcp >>= fun data ->
     let reply = match data with
       | Error e ->
@@ -192,9 +220,13 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
        | Error e ->
          Logs.err (fun m -> m "error %a sending redirect" Public.TCP.pp_write_error e))
     >>= fun () ->
+    http_access `Close;
     Public.TCP.close tcp
 
   let close tls tcp =
+    tls_access `Close;
+    frontend_access `Close;
+    backend_access `Close;
     Private.TCP.close tcp >>= fun () ->
     TLS.close tls
 
@@ -227,13 +259,18 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
         read_tcp_write_tls tcp tls
 
   let tls_accept priv config tls_config tcp_flow =
+    frontend_access `Open;
     (* TODO this should timeout the TLS handshake with a reasonable timer *)
     TLS.server_of_flow tls_config tcp_flow >>= function
     | Error e ->
       Logs.warn (fun m -> m "TLS error %a" TLS.pp_write_error e);
+      frontend_access `Close;
       Public.TCP.close tcp_flow
     | Ok tls_flow ->
+      tls_access `Open;
       let close () =
+        tls_access `Close;
+        frontend_access `Close;
         TLS.close tls_flow
       in
       match TLS.epoch tls_flow with
@@ -243,14 +280,16 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
           match
             match epoch.Tls.Core.own_name with
             | None ->
-              Logs.warn (fun m -> m "no server name specified");
+              Metrics.add snis (fun x -> x) (fun d -> d "no sni");
               default ()
             | Some sni ->
               match M.find_opt sni config.sni with
               | None ->
                 Logs.warn (fun m -> m "server name %s not configured" sni);
                 default ()
-              | Some (host, port) -> Some (host, port)
+              | Some (host, port) ->
+                Metrics.add snis (fun x -> x) (fun d -> d sni);
+                Some (host, port)
           with
           | None -> close ()
           | Some (host, port) ->
@@ -260,6 +299,7 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
                            Private.TCP.pp_error e);
               close ()
             | Ok tcp_flow ->
+              backend_access `Open;
               Lwt.join [
                 read_tls_write_tcp tls_flow tcp_flow ;
                 read_tcp_write_tls tcp_flow tls_flow
@@ -270,8 +310,17 @@ module Main (R : Mirage_random.S) (T : Mirage_time.S) (Pclock : Mirage_clock.PCL
         close ()
 
   module D = Dns_certify_mirage.Make(R)(Pclock)(T)(Public)
+  module Monitoring = Monitoring_experiments.Make(T)(Management)
+  module Syslog = Logs_syslog_mirage.Udp(C)(Pclock)(Management)
 
-  let start _ () () block pub priv =
+  let start c _ () () block pub priv management =
+    let hostname = Key_gen.name () in
+    (match Key_gen.syslog () with
+     | None -> Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
+     | Some ip -> Logs.set_reporter (Syslog.create c management ip ~hostname ()));
+    (match Key_gen.monitor () with
+     | None -> Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
+     | Some ip -> Monitoring.create ~hostname ip management);
     read_configuration block >>= fun config ->
     Private.listen_tcp priv ~port:(Key_gen.configuration_port ())
       (config_change block config (Cstruct.of_string (Key_gen.key ())));
